fdab88f90b1f847bd3e18798e4b5a4b34667a5c9
KavinGam <bhaitigam71@gmail.com>
2025-04-23 20:10:03 +0530

    update

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,9 +2,9 @@
     <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo BASE_URL;
 echo 'index.php'; ?>">
-            <p class="fs-3 fw-semibold text-uppercase text-light">sibsagar university</p>
+            <p class="fs-3 fw-semibold text-uppercase text-light my-2"></p>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,7 +21,7 @@ echo 'add-room.php'; ?>">Add Room</a>
                 <a class="nav-link active text-light active" aria-current="page" href="<?php echo BASE_URL;
 echo 'view-room.php'; ?>">View Room</a>
                 <a class="nav-link active text-light active" aria-current="page" href="<?php echo BASE_URL;
-echo 'department.php'; ?>">Manage Department</a>
+echo 'manage-department.php'; ?>">Manage Department</a>
                 <a href="<?php echo BASE_URL;
 echo 'advance-seat-plan.php'; ?>" aria-current="page" class="nav-link active text-light active">Advance Seat Plan</a>
 
